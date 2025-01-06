92199198c0084225cbd1bf40a3e1717e781bea45
subham-proj <3133ss32@gmail.com>
2025-01-07 00:20:35 +0530

    removed user.email mfield validation

--- models/userModel.js
+++ models/userModel.js
@@ -1,24 +1,11 @@
 import mongoose from "mongoose";
-import bcrypt from "bcryptjs";
-
-let validateEmail = function (email) {
-  let re = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
-  return re.test(email);
-};
 
 const userSchema = mongoose.Schema(
   {
     email: {
       type: String,
-      trim: true,
-      lowercase: true,
+      required: true,
       unique: true,
-      required: "Email address is required",
-      validate: [validateEmail, "Please fill a valid email address"],
-      match: [
-        /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/,
-        "Please fill a valid email address",
-      ],
     },
     name: {
       type: String,
